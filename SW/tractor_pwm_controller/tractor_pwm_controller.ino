a79de976b45220e380f612cb4c373a9e7fbe97ea
Dennis <d.lipschinski@gmail.com>
2025-01-07 20:38:57 +0100

    final HW version

--- SW/tractor_pwm_controller/tractor_pwm_controller.ino
+++ SW/tractor_pwm_controller/tractor_pwm_controller.ino
@@ -2,18 +2,21 @@
 #define D1 5
 #define D2 4
 #define D3 0
+#define D6 12
 #define D5 14
 #define BLUE_LED_PIN 2
 
-#define DIRECTION_IN_PIN D1
+#define DIRECTION_DRIVE_PIN D1
+#define DIRECTION_REVERSE_PIN D6
 #define DIRECTION_OUTA_PIN D2
 #define DIRECTION_OUTB_PIN D3
 #define PEDAL_PIN A0
 #define PWM_OUT_Pin D5
 
-#define PEDAL_THRESHHOLD 15
-#define PEDAL_OFFSET 200
+#define PEDAL_THRESHHOLD 10
+#define PEDAL_OFFSET 215
 #define PEDAL_SCALE 4
+#define MAX_PWM_VALUE 200
 
 int pedalValue = 0;
 int pwmValue = 0;
@@ -32,14 +35,15 @@ int heartbeat_led = LOW;
 int count = 0;
 
 void update() {
+  int cur_direction, diff;
+  cur_direction = !digitalRead(DIRECTION_DRIVE_PIN) + ((!digitalRead(DIRECTION_REVERSE_PIN))<<1);
   if(count>20){
     count =0;
     char s [80];
-    sprintf (s, "PWM value %d, dir: %d\n", pwmValue, direction);
+    sprintf (s, "PWM value %d, dir: %d %d %d\n", pwmValue, cur_direction,digitalRead(DIRECTION_DRIVE_PIN), digitalRead(DIRECTION_REVERSE_PIN));
     Serial.print(s);
     heartbeat_led = !heartbeat_led;
     if(driveState == IDLE){
-
       digitalWrite(BLUE_LED_PIN, 1);  // Turn the LED on
     }else{
       digitalWrite(BLUE_LED_PIN, heartbeat_led);  // Toggle when driving
@@ -54,23 +58,39 @@ void update() {
   } else {
     pedalValue = (pedalValue-PEDAL_OFFSET)/PEDAL_SCALE;
   }
-  if(digitalRead(DIRECTION_IN_PIN) != direction){
-    direction = !direction;
+  
+  if(cur_direction != direction){
+    direction = cur_direction;
     driveState = STOPPING;
   }
   switch(driveState){
     case IDLE:
-      if (pedalValue > PEDAL_THRESHHOLD) {
+      if (pedalValue > PEDAL_THRESHHOLD && direction>0 && direction<3) {
         driveState = DRIVING;
       } else {
         pwmValue = 0;
       }
     break;
     case DRIVING:
+      
       if(pwmValue < pedalValue){
-        pwmValue ++;
+        diff = pedalValue - pwmValue;
+        if(diff>50){
+          pwmValue += 50;
+        } else if(diff>10){
+          pwmValue += 10;
+        } else {
+          pwmValue ++;
+        }
       } else if(pwmValue > pedalValue) {
-        pwmValue --;
+        diff = pwmValue - pedalValue;
+        if(diff>50){
+          pwmValue -= 50;
+        } else if(diff>10){
+          pwmValue -= 10;
+        } else {
+          pwmValue --;
+        }
       } else if (pedalValue < PEDAL_THRESHHOLD) {
         driveState = IDLE;
         pwmValue = 0;
@@ -78,9 +98,14 @@ void update() {
     break;
     case STOPPING:
       if(pwmValue == 0){
-        digitalWrite(DIRECTION_OUTA_PIN,direction);
-        digitalWrite(DIRECTION_OUTB_PIN,!direction);
-        if (pedalValue > PEDAL_THRESHHOLD) {
+        if( direction == 1){
+          digitalWrite(DIRECTION_OUTA_PIN, 1);
+          digitalWrite(DIRECTION_OUTB_PIN, 0);
+        } else if(direction == 2){
+          digitalWrite(DIRECTION_OUTA_PIN, 0);
+          digitalWrite(DIRECTION_OUTB_PIN, 1);
+        }
+        if (pedalValue > PEDAL_THRESHHOLD && direction>0 && direction<3) {
           driveState = DRIVING;
         } else {
           driveState = IDLE;
@@ -91,18 +116,19 @@ void update() {
     break;
   }
   
-  analogWrite(PWM_OUT_Pin, _min(pwmValue,255));
+  analogWrite(PWM_OUT_Pin, _min(pwmValue,MAX_PWM_VALUE));
 }
 
 void setup() {
-  pinMode(DIRECTION_IN_PIN,INPUT_PULLUP);
+  pinMode(DIRECTION_DRIVE_PIN,INPUT_PULLUP);
+  pinMode(DIRECTION_REVERSE_PIN,INPUT_PULLUP);
   pinMode(BLUE_LED_PIN, OUTPUT);
   pinMode(DIRECTION_OUTA_PIN, OUTPUT);
   pinMode(DIRECTION_OUTB_PIN, OUTPUT);
   pinMode(PWM_OUT_Pin, OUTPUT);
   Serial.begin(74880); 
   Serial.print("Booted \n");
-  update_analog.attach(0.01, update);  
+  update_analog.attach(0.05, update);  
 }
 
 // the loop function runs over and over again forever
